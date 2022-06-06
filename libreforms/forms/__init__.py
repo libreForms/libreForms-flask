b64f238f5523e00e07651aec63bd3886c95323ac
Sig Janoska-Bedi <54920695+signebedi@users.noreply.github.com>
2022-06-06 09:23:50 -0500

    Update __init__.py

--- libreforms/forms/__init__.py
+++ libreforms/forms/__init__.py
@@ -1,32 +1,80 @@
-# forms/__init__.py: the source for form field data in the libreForm application.
-# the data contained in this file can be referenced from the base app directory by
-# running 'import forms' in a python script. The data here should be stored in the 
-# 'forms' variable, which is a dictionary whose keys correspond to the name and/or
-# number of a given form (meaning no repetition is allowed), while the values is a 
-# nested dictionary whose keys correspond to a given form field, while the values
-# correspond to the default value of that field, which helps also define the data
-# type by leveraging python's dynamic typing system. At this time, only strings and
-# integers are supported; if the datatype does not neatly fit into one of these types,
-# then the field generator in the libreForms app will treat the data like a string.
-# Also note, field names may be capitalized, but spaces should be replaced by under
-# scores (_) to ensure they are computer-friendly; these are replaced with spaces in 
-# HTML/Jinja2 template for human-readability.
+# libreforms/forms/__init__.py: the source for form field data in the libreForms application.
+# libreForms defines a robust abstraction layer between (1) the types of form fields that are 
+# used to collect user data and (2) the data type that the content of these form fields take, 
+# once submitted by the user, and (3) the underlying data structure of form responses, which 
+# the system stores in a JSON-like database well suited to integration with useful data science 
+# tools like pandas, as well as visualization libraries like plotly. Specifically, this API
+# supports the following form fields:
+    # "text"
+    # "password"
+    # "radio"
+    # "checkbox" 
+    # "date"
+    # "hidden"
+    # "number"
+    # "file"
+# as well as the following output data types:
+    # str
+    # float
+    # int
+# input_field refers only to the structure of the markup field that will be used
+# to collect the data; all the information regarding the typing and validation
+# of the data exists in output_data.
+# for each form, optional, non-form-field data is defined with an underscore (_)
+# preceding the key name, like _allow_repeats. All of these are optional fields 
+# and default to a value of False. 
+
+import datetime
 
 forms = {
-    "sample-1": {                   # this structure creates a dictionary of keys corresponding to
-        "Preparer":"Bill Smith",    # each unique form name that an organization's sys admin would
-        "Current_Units":0,          # like to make available to their users, while the value is a
-        "Requested_Units":0,        # second, nested dictionary containing the field names (keys)
-        "Item_ID":"003A9D",         # mapped to their default values (values); by setting default
-        "Another_Int":0,            # values, we can easily identify the data type of the field;
-        "Yet_Another_Int":0,        # and if you'd like to suppress default values on the web page
-        "Another_String":'NA',      # you can simply set display_default_values=False in app.py.
-        "allow_repeat":False,       # the "allow_repeat" field is dropped before being returned, 
-    },                              # and allows the user to to add an arbitrary number of add'l 'rows' of data
-    "sample-2": {             
-        "Preparer_Team":["Infrastructure", "Finance", "Human Capital"],      
-        "Preparer":"Bill Smith",         
-        "Access_Requested":["High", "Low"], 
-        "allow_repeat":True
+    "sample-form": {
+        "Text_Field": {
+            "input_field": {"type": "text", "content": ["NA"]},
+            "output_data": {"type": "str", "validators": [lambda p: len(p) >= 6]},
+        },
+        "Pass_Field": {
+            "input_field": {"type": "password", "content": [""]},
+            "output_data": {"type": "str", "validators": []},
+        },
+        "Radio_Field": {
+            "input_field": {"type": "radio", "content": ["Pick", "An", "Option"]},
+            "output_data": {"type": "str", "validators": []},
+        },
+        "Check_Field": {
+            "input_field": {"type": "checkbox", "content": ["Pick", "An", "Option"]},
+            "output_data": {"type": "str", "validators": []},
+        },
+        "Date_Field": {
+            "input_field": {"type": "date", "content": [datetime.datetime.today().strftime("%Y-%m-%d")]},
+            # "input_field": {"type": "date", "content": []},
+            "output_data": {"type": "date", "validators": []},
+        },
+        "Hidden_Field": {
+            "input_field": {"type": "hidden", "content": ["This field is hidden"]},
+            "output_data": {"type": "str", "validators": []},
+        },
+        "Float_Field": {
+            "input_field": {"type": "number", "content": [0]},
+            "output_data": {"type": "float", "validators": []},
+        }, 
+        "Int_Field": {
+            "input_field": {"type": "number", "content": [0]},
+            "output_data": {"type": "int", "validators": []},
+        }, 
+#         "File_Field": {
+#             "input_field": {"type": "file", "content": [None]}, # still need to review https://flask.palletsprojects.com/en/2.1.x/patterns/fileuploads/
+#             "output_data": {"type": TBD, "validators": []},
+#         },
+        "_dashboard": {             # defaults to False
+            "type": "scatter",      # this is a highly powerful feature but requires
+            "fields": {             # some knowledge of plotly dashboards; currently
+                "x": "Timestamp",   # only line charts with limited features supported
+                "y": "Num_Field", 
+                "color": "Text_Field"
+            }
+        },
+        "_allow_repeat": False, # defaults to False
+        "_allow_uploads": False, # defaults to False
+        "_allow_csv_templates": False, # defaults to False
     },
 }
