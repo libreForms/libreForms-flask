1a8dbd0f75cebed87651242b3b7627ad13744175
signebedi <signe@siftingwinnowing.com>
2022-06-20 14:00:33 -0500

    Modified: setup.py to include add'l install requires

--- setup.py
+++ setup.py
@@ -2,11 +2,17 @@ from setuptools import find_packages, setup
 
 setup(
     name='libreforms',
-    version='1.0.0',
+    version='0.0.1',
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
     install_requires=[
         'flask',
+        'gunicorn',
+        'pandas',
+        'plotly',
+        'pymongo',
+        'pytest',
+        'webargs',
     ],
 )
