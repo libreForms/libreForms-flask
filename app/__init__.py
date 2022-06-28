b174e9b7c89402ca99431817cb4b11319a18645a
signebedi <signe@siftingwinnowing.com>
2022-06-28 16:43:38 -0500

    Added: some add'l bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ import os
 from flask import Flask, render_template
 import app.log_functions
 from flask_admin import Admin
+from flask_bootstrap import Bootstrap5
 
 # if application log path doesn't exist, make it
 if not os.path.exists ("log/"):
@@ -45,12 +46,14 @@ def create_app(test_config=None):
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'app.sqlite'),
         FLASK_ADMIN_SWATCH='darkly',
+        BOOTSTRAP_BOOTSWATCH_THEME='darkly',
     )
 
 
     admin = Admin(app, name='libreForms', template_mode='bootstrap4')
     # Add administrative views here
 
+    bootstrap = Bootstrap5(app)
 
     if os.path.exists ("secret_key"):
         with open("secret_key", "r+") as f:
