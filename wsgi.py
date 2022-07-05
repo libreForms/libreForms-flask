e6f4f0d0ce16e1e20036d389d6d9f108115554f3
signebedi <signe@siftingwinnowing.com>
2022-07-04 21:04:36 -0500

    Added: default user support in wsgi

--- wsgi.py
+++ wsgi.py
@@ -1,22 +1,30 @@
 # a simple wsgi script to run the flask application using gunicorn
 
-from app import create_app, db, create_app, models
+from app import create_app, db
+from app.models import User
+
+# initialize the app
+app = create_app()
+
+# push the app context
+# see https://flask-sqlalchemy.palletsprojects.com/en/2.x/contexts/
+app.app_context().push()
 
 # initialize the database
 db.create_all(app=create_app())
 
 # create default user
-# initial_user = models.User(
-#                 username='libreforms', 
-#                 password='pbkdf2:sha256:260000$nQVWxd59E8lmkruy$13d8c4d408185ccc3549d3629be9cd57267a7d660abef389b3be70850e1bbfbf',
-#                 created_date='2022-06-01 00:00:00',
-#         )
+initial_user = User(
+                id=1,
+                username='libreforms', 
+                password='pbkdf2:sha256:260000$nQVWxd59E8lmkruy$13d8c4d408185ccc3549d3629be9cd57267a7d660abef389b3be70850e1bbfbf',
+                created_date='2022-06-01 00:00:00',
+        )
 
-# db.session.add(initial_user)
-# db.session.commit()
+db.session.add(initial_user)
+db.session.commit()
 
-# initialize the app
-app = create_app()
+
 
 if __name__ == "__main__":
         app.run()
