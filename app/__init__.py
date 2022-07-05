d6e346cdad92c828735251aa8e16e42d17c81b4e
signebedi <signe@siftingwinnowing.com>
2022-07-04 22:27:49 -0500

    Routine: syntax cleanup

--- app/__init__.py
+++ app/__init__.py
@@ -90,32 +90,29 @@ def create_app(test_config=None):
             user=current_user if current_user.is_authenticated else None,
         )
 
+    from .models import User
 
     # initialize the database
     db.init_app(app=app)
 
     # create the database if it doesn't exist
-    db.create_all(app=app) if not os.path.exists(os.path.join('instance','app.sqlite')) else None
+    if not os.path.exists(os.path.join('instance','app.sqlite')):
+        db.create_all(app=app)
 
-    from app.models import User
-
-    # create default user if doesn't exist
-    with app.app_context():
-        if db.session.query(User).filter_by(username='libreforms').count() < 1:
-            initial_user = User(
-                    id=1,
-                    username='libreforms', 
-                    password='pbkdf2:sha256:260000$nQVWxd59E8lmkruy$13d8c4d408185ccc3549d3629be9cd57267a7d660abef389b3be70850e1bbfbf',
-                    created_date='2022-06-01 00:00:00',
-                )
-            db.session.add(initial_user)
-            db.session.commit()
+        # create default user if doesn't exist
+        # solution from https://stackoverflow.com/a/39288652
+        with app.app_context():
+            if db.session.query(User).filter_by(username='libreforms').count() < 1:
+                initial_user = User(id=1,
+                                    username='libreforms', 
+                                    password='pbkdf2:sha256:260000$nQVWxd59E8lmkruy$13d8c4d408185ccc3549d3629be9cd57267a7d660abef389b3be70850e1bbfbf',
+                                    created_date='2022-06-01 00:00:00',)
+                db.session.add(initial_user)
+                db.session.commit()
 
     login_manager = LoginManager()
     login_manager.login_view = 'auth.login'
     login_manager.init_app(app)
-
-    from .models import User
 
     @login_manager.user_loader
     def load_user(id):
