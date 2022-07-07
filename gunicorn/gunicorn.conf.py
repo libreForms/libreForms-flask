46977c9fb0c2cf6d74808e61b66439aae938ea3c
signebedi <signe@siftingwinnowing.com>
2022-07-07 18:21:38 -0500

    Added: log cleanup and general improvements in gunicorn.conf.py

--- gunicorn/gunicorn.conf.py
+++ gunicorn/gunicorn.conf.py
@@ -1,14 +1,20 @@
+# gunicorn.conf.py: the Gunicorn configuration file for the libreForms web application
+# References: 
+# https://github.com/benoitc/gunicorn/issues/2136
+# https://github.com/benoitc/gunicorn/blob/master/examples/example_config.py
+# 
+
 import os, re
 
-def on_starting(logpath='/opt/libreforms/log'):
+def pre_fork(server, worker):
     for log in os.listdir(logpath):
         if re.fullmatch(r"libreforms-[0-9]+.log", log):
                 os.remove (os.path.join(logpath, log))
 
+bind="0.0.0.0:8000"
 workers = 3 
-errorlog = "/opt/libreForms/log/gunicorn.error"
-accesslog = "/opt/libreForms/log/gunicorn.access" 
+logpath='/opt/libreForms/log'
+errorlog = os.path.join(logpath, "gunicorn.error")
+accesslog = os.path.join(logpath, "gunicorn.access")
 loglevel = "debug"
 
-# Server Hooks
-on_starting = on_starting()
