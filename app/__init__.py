5e277966e6f299880f08333b29f18115d9f5a36c
signebedi <signe@siftingwinnowing.com>
2022-07-07 20:07:09 -0500

    Externalize: display artifacts

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,7 @@ import app.log_functions
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, current_user
 from libreforms import __version__
+from app.display import display
 
 db = SQLAlchemy()
 
@@ -19,30 +20,6 @@ if not os.path.exists ("log/"):
 
 log = app.log_functions.set_logger('log/libreforms.log',__name__)
 log.info('LIBREFORMS - started libreforms web application.')
-
-# define default page display and,
-# if a site_overrides file exists, 
-# use it to overwrite defaults
-
-display = {}
-display['site_name'] = "libreForms"
-display['homepage_msg'] = "Welcome to libreForms, an extensible form building abstraction \
-                            layer implemented in Flask. Select a view from above to get started. \
-                            Review the docs at https://github.com/signebedi/libreForms."
-display['warning_banner'] = "" 
-display['theme'] = "" # unused
-display['favicon'] = "" # unused
-display['image'] = "" # unused
-display['favicon'] = "default_favicon.ico" 
-display['default_org'] = "" 
-display['version'] =__version__
-
-if os.path.exists ("app/site_overrides.py"):
-    import app.site_overrides
-    for config in app.site_overrides.display.keys():
-        display[config] = app.site_overrides.display[config]
-    log.info('LIBREFORMS - found a site overrides file.')
-
 
 def create_app(test_config=None):
  
---
+++ app/display.py
@@ -0,0 +1,25 @@
+# define default page display and,
+# if a display_overrides file exists, 
+# use it to overwrite defaults
+import os
+from libreforms import __version__
+
+display = {}
+display['site_name'] = "libreForms"
+display['homepage_msg'] = "Welcome to libreForms, an extensible form building abstraction \
+                            layer implemented in Flask. Select a view from above to get started. \
+                            Review the docs at <a href='https://github.com/signebedi/libreForms'> \
+                            https://github.com/signebedi/libreForms</a>."
+display['warning_banner'] = "" 
+display['theme'] = "" # unused
+display['favicon'] = "" # unused
+display['image'] = "" # unused
+display['favicon'] = "default_favicon.ico" 
+display['default_org'] = "" 
+display['version'] = __version__
+display['privacy_policy'] = ''
+
+if os.path.exists ("app/display_overrides.py"):
+    import app.display_overrides
+    for config in app.display_overrides.display.keys():
+        display[config] = app.display_overrides.display[config]
