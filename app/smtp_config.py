b0b8d0ff064477b0ac4a91213652368a298d41db
signebedi <signe@siftingwinnowing.com>
2022-09-22 19:11:45 -0500

    Added: preliminary SMTP logic

--- app/smtp_config.py
+++ app/smtp_config.py
@@ -1,43 +1,30 @@
-import hashlib, os, trace, sys, logging, subprocess, smtplib, time
-from email.message import EmailMessage
-from email.mime.multipart import MIMEMultipart
-from email.mime.base import MIMEBase
-from email.mime.text import MIMEText
-from email.utils import COMMASPACE, formatdate
-from email import encoders
+import os
+import ssl
+import smtplib 
 import datetime as dt
 
-class sendMail():
-    def __init__(self, mail_server=None, port=None, 
-                        username=None, password=None):
-        # this is where we initialize the connection to the mail server.
+# borrowed shamelessly from 
+# https://www.aabidsofi.com/posts/sending-emails-with-aws-ses-and-python/
 
-        self.s = smtplib.SMTP(mail_server, port)
+def send_email_with_ses(from_address, to_address, content,
+                            mail_server, port,username, password):
 
-        # but... should the directives below come in the send_mail method?
-        self.s.starttls() 
-        self.s.login(username, password)
+# getting the credentials fron evironemnt
+# host = os.environ.get("SES_HOST_ADDRESS")
+# user = os.environ.get("SES_USER_ID")
+# password = os.environ.get("SES_PASSWORD")
 
-    # this function suppresses the log by default but otherwise permits
-    # its user to designate a log file to write its success
-    def send_mail(self, subject, content, from_address, to_address, attachment=None, logfile=None):
-        _from = from_address
-        _to = to_address
-        msg = MIMEMultipart()
-        msg['Subject'] = subject
-        msg['From'] = _from
-        msg['To'] = _to
-        msg.attach(MIMEText(content))
+    # setting up ssl context
+    context = ssl.create_default_context()
 
-        if attachment != None:
-            part = MIMEBase('application', "octet-stream")
-            with open(attachment, 'rb') as file:
-                part.set_payload(file.read())
-            encoders.encode_base64(part)
+    # creating an unsecure smtp connection
+    with smtplib.SMTP(mail_server,port) as server :
 
-            part.add_header('Content-Disposition', 'attachment; filename="{}"'.format(os.path.basename(attachment)))
-            msg.attach(part)
+        # securing using tls
+        server.starttls(context=context)
 
-        self.s.sendmail(_from, _to, msg.as_string())
-        self.s.quit()
-        if logfile: logfile.info(f'successfully sent an email update to {to_address}\n')
+        # authenticating with the server to prove our identity
+        server.login(username, password)
+
+        # sending a plain text email
+        server.sendmail(from_address, to_address, content)
