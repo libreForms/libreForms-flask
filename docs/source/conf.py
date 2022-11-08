16aea65601812498b4a8396bed658e34771a0acd
signebedi <signe@siftingwinnowing.com>
2022-11-07 18:10:16 -0600

    Modified: version > 0.8.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = '2022, Sig Janoska-Bedi'
 author = 'Sig Janoska-Bedi'
 
 # The full version, including alpha/beta/rc tags
-release = '0.7.0'
+release = '0.8.0'
 
 
 # -- General configuration ---------------------------------------------------
