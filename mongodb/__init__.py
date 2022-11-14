d46823d72b33fa19fb2dd9330034591a4163c084
signebedi <signe@siftingwinnowing.com>
2022-11-14 08:23:56 -0600

    Modified: mongodb behavior to write modifications to the Journal after post-submission edits

--- mongodb/__init__.py
+++ mongodb/__init__.py
@@ -21,12 +21,23 @@ class MongoDB:
         self.client = MongoClient(host, port)
         self.db = self.client['libreforms']
 
-    def write_document_to_collection(self, data, collection_name, reporter=None):
+    def write_document_to_collection(self, data, collection_name, 
+                                                    reporter=None,
+                                                    # the `modifications` kwarg expects a dict of 
+                                                    # fields and the values they've been changed to,
+                                                    # else Nonetype to signify an initial submission.
+                                                    modifications=None):
         import datetime
         collection = self.db[collection_name]
         data['Timestamp'] = str(datetime.datetime.utcnow())
         data['Reporter'] = str(reporter) if reporter else None
-        data['Journal'] = {data['Timestamp']:f"{data['Reporter']} created initial submission."}
+        
+        # here we define the behavior of the `Journal` metadata field 
+        if not modifications:
+            data['Journal'] = {data['Timestamp']:f"{data['Reporter']} created initial submission."}
+        else:
+            data['Journal'] = {data['Timestamp']:f"{data['Reporter']} made the following modifications: {modifications}."}
+
         collection.insert_one(data).inserted_id
 
     def read_documents_from_collection(self, collection_name):
