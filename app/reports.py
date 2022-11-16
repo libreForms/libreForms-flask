8c31d4a7920da6972c268ffd2598abdfa5de424a
signebedi <signe@siftingwinnowing.com>
2022-11-16 15:51:17 -0600

    Grumble: adding temp file system and add'l conditionality to report logic

--- app/reports.py
+++ app/reports.py
@@ -1,21 +1,30 @@
 from crontab import CronTab
-from app import db, log, mailer, display
+from app import db, log, mailer, display, tempfile_path
 
 
 class reportHandler():
     def __init__(self, ) -> None:
+
+        # we create a local key mapping that will each correspond to a 
+        # event handling object
         self.jobs = {}
 
+    # this code is a bit messy. We start by writing timed reports from the
+    # 'send_reports' display configuration to the system crontab  
     def set_cron_jobs(self, job_list=display['send_reports']):
 
         for key in job_list.keys():
 
-            with CronTab(user=True) as cron:
-                self.jobs[key] = cron.new(command='echo hello_world')
-                self.jobs[key].setall(job_list[key]['trigger'])
+            if job_list[key]['type'] == 'timed':
 
-            log.info(f'LIBREFORMS - wrote {key} report to CRON.')
+                with CronTab(user=True) as cron:
+                    self.jobs[key] = cron.new(command='echo hello_world')
+                    self.jobs[key].setall(job_list[key]['trigger'])
 
+                log.info(f'LIBREFORMS - wrote {key} report to CRON.')
+
+            else:
+                self.jobs[key] = None
 
 
 
