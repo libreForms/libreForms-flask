c8f0ae71c279fee7e0d3b4b17a5252431e678603
signebedi <signe@siftingwinnowing.com>
2022-12-04 10:18:16 -0600

    Added: boilerplate for app.display (#78)

--- app/display.py
+++ app/display.py
@@ -1,3 +1,18 @@
+""" 
+display.py: collection of default app configs
+
+
+
+"""
+
+__name__ = "app.display"
+__author__ = "Sig Janoska-Bedi"
+__credits__ = ["Sig Janoska-Bedi"]
+__version__ = "1.0"
+__license__ = "AGPL-3.0"
+__maintainer__ = "Sig Janoska-Bedi"
+__email__ = "signe@atreeus.com"
+
 # define default page display and,
 # if a display_overrides file exists, 
 # use it to overwrite defaults
