70169dbcd5286c2540e10b005e6b0cb46ac88af0
signebedi <signe@siftingwinnowing.com>
2022-12-04 10:16:00 -0600

    Added: boilerplate for app.ldap (#78)

--- app/ldap.py
+++ app/ldap.py
@@ -1,3 +1,18 @@
+""" 
+ldap.py: ldap auth integration
+
+
+
+"""
+
+__name__ = "app.ldap"
+__author__ = "Sig Janoska-Bedi"
+__credits__ = ["Sig Janoska-Bedi"]
+__version__ = "1.0"
+__license__ = "AGPL-3.0"
+__maintainer__ = "Sig Janoska-Bedi"
+__email__ = "signe@atreeus.com"
+
 import ldap
 
 class initLDAP():
