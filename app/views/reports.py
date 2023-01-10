88885360be1d6deaa5c174c1e1c6c749a438abb9
signebedi <signe@siftingwinnowing.com>
2023-01-10 13:52:33 -0600

    Added: initial `reports/` route and jinja2 template (#73)

--- app/views/reports.py
+++ app/views/reports.py
@@ -72,6 +72,7 @@ from markupsafe import Markup
 
 # import custom packages from the current repository
 import libreforms as libreforms
+from app.views.forms import form_menu
 from app.views.auth import login_required
 from app import config, log, mongodb
 
@@ -143,34 +144,48 @@ class reportManager():
 
 bp = Blueprint('reports', __name__, url_prefix='/reports')
 
-@bp.route(f'/reports', methods=['GET', 'POST'])
+# show list of current reports, and interface to create new ones
+@bp.route(f'/', methods=['GET', 'POST'])
 @login_required
 def reports():
-    pass # show list of current reports, and interface to create new ones
 
-@bp.route(f'/reports/create', methods=['GET', 'POST'])
+    # generate a list of reports, and render a list of links to these here. 
+    # dim those that are inactive
+
+    return render_template('app/reports.html', 
+            notifications=current_app.config["NOTIFICATIONS"]() if current_user.is_authenticated else None,
+            name="Reports",
+            type="reports",
+            config=config,
+            user=current_user,
+        ) 
+
+
+
+
+@bp.route(f'/create', methods=['GET', 'POST'])
 @login_required
 def create_reports():
     pass # create a new report
 
-@bp.route(f'/reports/<report_id>', methods=['GET', 'POST'])
+@bp.route(f'/<report_id>', methods=['GET', 'POST'])
 @login_required
 def manage_reports(report_id):
     pass # modify existing report
 
 
-@bp.route(f'/reports/<report_id>/activate', methods=['GET', 'POST'])
+@bp.route(f'/<report_id>/activate', methods=['GET', 'POST'])
 @login_required
 def activate_report(report_id):
     pass # activate existing report
 
 
-@bp.route(f'/reports/<report_id>/deactivate', methods=['GET', 'POST'])
+@bp.route(f'/<report_id>/deactivate', methods=['GET', 'POST'])
 @login_required
 def deactivate_report(report_id):
     pass # deactivate existing report
 
-@bp.route(f'/reports/<report_id>/send', methods=['GET', 'POST'])
+@bp.route(f'/<report_id>/send', methods=['GET', 'POST'])
 @login_required
 def send_report(report_id):
     pass # send existing report now
