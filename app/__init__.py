7ebd634f938162d2be840555ca53c6506cd0b51b
signebedi <signe@siftingwinnowing.com>
2023-01-14 19:20:52 -0600

    Removed: celery beat start method (#182)

--- app/__init__.py
+++ app/__init__.py
@@ -370,9 +370,6 @@ def create_app(test_config=None):
         # sender.add_periodic_task(3600.0, send_reports.s(reports), name='send reports periodically')
         sender.add_periodic_task(1.0, celery_beat_logger.s(), name='log that celery beat is working')
 
-    # start the celery beat service
-    celery.beat.start()
-
 
     # create a task status endpoint
     @app.route('/status/<task_id>', methods=['GET'])
