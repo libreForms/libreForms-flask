41f161b97c6ffa6201b7a4c35671e6a7b0293a2a
signebedi <signe@siftingwinnowing.com>
2023-01-18 13:47:50 -0600

    Added: try / except error handling for dashboards

--- app/views/dashboards.py
+++ app/views/dashboards.py
@@ -68,79 +68,81 @@ def dashboards(form_name):
         flash('Your system administrator has not enabled any dashboards for this form.')
         return redirect(url_for('dashboards.dashboards_home'))
 
-    
-    data = mongodb.read_documents_from_collection(form_name)
-    df = pd.DataFrame(list(data))
-    ref = libreforms.forms[form_name]["_dashboard"]['fields']
+    try:
 
-    # here we allow the user to specify the field they want to use, 
-    # overriding the default y-axis field defined in libreforms/forms.
-    # warning, this may be buggy
+        data = mongodb.read_documents_from_collection(form_name)
+        df = pd.DataFrame(list(data))
+        ref = libreforms.forms[form_name]["_dashboard"]['fields']
 
-    # if request.args.get("y") and request.args.get("y") in form.keys(): # alternative if we want to verify the field exists
-    y_context = request.args.get("y") if request.args.get("y") else ref['y']
+        # here we allow the user to specify the field they want to use, 
+        # overriding the default y-axis field defined in libreforms/forms.
+        # warning, this may be buggy
+
+        # if request.args.get("y") and request.args.get("y") in form.keys(): # alternative if we want to verify the field exists
+        y_context = request.args.get("y") if request.args.get("y") else ref['y']
 
 
-    if len(df.index) < 1:
-        flash('This form has not received any submissions.')
+        if len(df.index) < 1:
+            flash('This form has not received any submissions.')
+            return redirect(url_for('dashboards.dashboards_home'))
+
+        theme = 'plotly_dark' if (config['dark_mode'] and \
+            not current_user.theme == 'light') or current_user.theme == 'dark' else 'plotly_white'
+
+
+        viz_type = libreforms.forms[form_name]["_dashboard"]['type']
+        if viz_type == "scatter":
+            fig = px.scatter(df, 
+                        x=ref['x'], 
+                        y=y_context, 
+                        color=ref['color'],
+                        template=theme)
+            graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+
+        elif viz_type == "bar":
+            fig = px.histogram(df, 
+                        x=ref['x'], 
+                        y=y_context, 
+                        barmode='group',
+                        color=ref['color'],
+                        template='plotly_dark')
+            graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+
+        elif viz_type == "histogram":
+            fig = px.histogram(df, 
+                        x=ref['x'], 
+                        y=y_context, 
+                        color=ref['color'],
+                        template='plotly_dark')
+            graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+
+
+        elif viz_type == "table":
+            pass
+
+        else: # default to line graph
+            fig = px.line(df, 
+                        x=ref['x'], 
+                        y=y_context, 
+                        color=ref['color'],
+                        template='plotly_dark')
+            graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+
+
+        return render_template('app/dashboards.html', 
+            graphJSON=graphJSON,
+            name=form_name,
+            type="dashboards",
+            site_name=config['site_name'],
+            notifications=current_app.config["NOTIFICATIONS"]() if current_user.is_authenticated else None,
+            menu=form_menu(checkDashboardGroup),
+            options=propagate_form_configs(form=form_name),
+            config=config,
+            user=current_user,
+        )
+    except:
+        
+        flash('This dashboard does not exist.')
         return redirect(url_for('dashboards.dashboards_home'))
-
-    theme = 'plotly_dark' if (config['dark_mode'] and \
-        not current_user.theme == 'light') or current_user.theme == 'dark' else 'plotly_white'
-
-
-    viz_type = libreforms.forms[form_name]["_dashboard"]['type']
-    if viz_type == "scatter":
-        fig = px.scatter(df, 
-                    x=ref['x'], 
-                    y=y_context, 
-                    color=ref['color'],
-                    template=theme)
-        graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
-
-    elif viz_type == "bar":
-        fig = px.histogram(df, 
-                    x=ref['x'], 
-                    y=y_context, 
-                    barmode='group',
-                    color=ref['color'],
-                    template='plotly_dark')
-        graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
-
-    elif viz_type == "histogram":
-        fig = px.histogram(df, 
-                    x=ref['x'], 
-                    y=y_context, 
-                    color=ref['color'],
-                    template='plotly_dark')
-        graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
-
-
-    elif viz_type == "table":
-        pass
-
-    else: # default to line graph
-        fig = px.line(df, 
-                    x=ref['x'], 
-                    y=y_context, 
-                    color=ref['color'],
-                    template='plotly_dark')
-        graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
-
-
-    return render_template('app/dashboards.html', 
-        graphJSON=graphJSON,
-        name=form_name,
-        type="dashboards",
-        site_name=config['site_name'],
-        notifications=current_app.config["NOTIFICATIONS"]() if current_user.is_authenticated else None,
-        menu=form_menu(checkDashboardGroup),
-        options=propagate_form_configs(form=form_name),
-        config=config,
-        user=current_user,
-    )
-
-
-
 
 
