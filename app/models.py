06c8bba015763a9701d5508f6c3bd1ffc8070304
signebedi <signe@siftingwinnowing.com>
2023-01-24 11:44:13 -0600

    Modified: report data model to include enum `type` (#228)

--- app/models.py
+++ app/models.py
@@ -125,6 +125,7 @@ class Report(db.Model):
     form_name = db.Column(db.String(100))
     filters = db.Column(db.String(1000))
     frequency = db.Column(db.Enum('hourly', 'daily', 'weekly', 'monthly', 'annually', 'manual'))
+    type = db.Column(db.Enum('created_since_last_run', 'modified_since_last_run', 'created_all_time'))
     active = db.Column(db.Boolean)
     timestamp = db.Column(db.Float)
     timestamp_human_readable = db.Column(db.String(100))
