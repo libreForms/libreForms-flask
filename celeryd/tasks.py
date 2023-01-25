5edbc4160a55d670c888c87aa99972cc4441da04
signebedi <signe@siftingwinnowing.com>
2023-01-24 23:16:15 -0600

    Modified: celery task to use current_app (#228)

--- celeryd/tasks.py
+++ celeryd/tasks.py
@@ -21,6 +21,7 @@ __maintainer__ = "Sig Janoska-Bedi"
 __email__ = "signe@atreeus.com"
 
 from app import celery, log, mailer, mongodb
+from flask import current_app
 
 
 # here we define a tasks to send emails asynchonously - it's just a 
@@ -55,7 +56,7 @@ def write_document_to_collection_async(self, data, collection_name, reporter=Non
 
 @celery.task()
 def elasticsearch_index_document(body, id, index="submissions"):
-        app.elasticsearch.index(id, body, index=index)
+        current_app.elasticsearch.index(id, body, index=index)
         return True
     # expects data to be formulated as follows:
     # data = json.dumps({
