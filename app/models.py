6cd98ef7ca9f304fd22fd92d6fa17e7cf342fcb7
signebedi <signe@siftingwinnowing.com>
2023-02-04 19:43:41 -0600

    Modified: cleaned up app.models documentation

--- app/models.py
+++ app/models.py
@@ -1,22 +1,25 @@
 """ 
 models.py: defining the models for the application's SQL database
 
-This script defines the data models for two databases: (1) the user 
-database, which is used to store local auth data; (2) the signing
-database, which is used to store signatures / keys for various uses, 
-including RESTful API access, email verification, & password resets. 
+This script defines the data models for three tables: (1) the 
+user table, which is used to store local auth data; (2) the 
+signing table, which is used to store signatures / keys for 
+various use cases where strong authentication is not possible, 
+and we must therefore limit / scope authorization to eg. 
+RESTful API access, email verification, & password resets; 
+(3) the report table, which is used to store user-defined reports. 
 
-# User database
+# User table
 
-The code that implements this database is stored app/__init__.py 
-and app/auth.py. When Gunicorn is used, it will create the database
+The code that implements this table is stored app/__init__.py 
+and app/auth.py. When Gunicorn is used, it will create the table
 using behavior defined in etc/gunicorn.conf.py to avoid collision. 
 Specifically, the model creates a default user 'libreforms' for initial
 access to the web application. When used, Gunicorn will create this user
 before forking into multiple process to prevent errors from multiple 
 workers trying to create the same user. Further, if custom user fields 
 have been defined in the application's configuration file, these will 
-be added to the database at the point of instantiation, see 
+be added to the table at the point of instantiation, see 
 app/__init__.py and etc/gunicorn.conf.py.
 
 To avoid too many changes to the user data model while still allowing
@@ -28,12 +31,12 @@ should be aware of some default behavior: groups are allowed access to all
 forms, fields, dashboards, tables, and other resources by default unless 
 explicitly denied using a `deny_group_access` config at that resource's level.
 
-# Signing database
+# Signing table
 
-The code that implements this database is stored in app/signing.py, 
+The code that implements this table is stored in app/signing.py, 
 app/__init__.py, and app/auth.py. 
 
-The signing database sets the `signature` field to be the primary key. 
+The signing table sets the `signature` field to be the primary key. 
 This approach ensures that each signature is unique, and thus prevents
 'collisions' where a key is reused and allows some kind of leakage or
 improper access. Generally, we link an `email` (Nb. this is not a required
@@ -44,7 +47,7 @@ The `active` field is a bool that determines whether a signature has been
 'used up'. All signatures are made inactive when their `expiration` has 
 passed, while some will expire after a certain number of uses - this depends
 on the `scope` that has been defined, see app/signing.py for more information.
-Generally, a signature row will not be dropped from the database; instead, 
+Generally, a signature row will not be dropped from the table; instead, 
 its `active` field will be set to 0. This helps avoid a subset of potential 
 collisions where users who previously obtained a signature can reuse it to
 improperly access a future user's scope. This risk is exacerbated because
@@ -52,7 +55,7 @@ signatures are documented through users' emails and logs, meaning they
 may retain access to the signature after it has expired.
 
 
-# Report database
+# Report table
 
 Reports are implemented in app.reporting and views are generated in 
 app.views.reports. In the web application, there are two forms of reports:
@@ -61,7 +64,7 @@ which have customizable circulation; (2) user-defined reports, which are
 defined by users in the web application, and which only circulate reports
 to the requesting user. This data model only handles the latter.
 
-Reports stored in this database are given a unique `report_id`, which serves
+Reports stored in this table are given a unique `report_id`, which serves
 as the table's primary key. Each report is linked to the requesting `user_id`.
 The `frequency` field determined the send frequency (daily, weekly, monthly)
 Any logic conditions / filters are stored as strings in the `filters` field,
