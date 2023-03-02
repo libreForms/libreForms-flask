93a47c91896fab0ed7ce8903f2a91468efe62d53
signebedi <signe@siftingwinnowing.com>
2023-03-02 11:55:37 -0600

    Modified: restart_app_async runtime cmd (#255)

--- celeryd/tasks.py
+++ celeryd/tasks.py
@@ -61,7 +61,7 @@ def write_document_to_collection_async(self, data, collection_name, reporter=Non
 def restart_app_async(type='gunicorn'):
     if type == 'gunicorn': # we leave the door open here to support run methods other than gunicorn ...
         # here we try to restart gunicorn ... which might not be in use ...
-        os.system("ps aux | grep libreforms-gunicorn | awk '{ print $2 }' | xargs kill -HUP")
+        os.system("ps aux | grep gunicorn | awk '{ print $2 }' | xargs kill -HUP")
         # os.system('systemctl restart libreforms-gunicorn')
 
 
