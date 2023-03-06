c0cf1de7e04f4a78f5c305f7b79ae4b95518203b
signebedi <signe@siftingwinnowing.com>
2023-03-05 20:10:34 -0600

    Added: full useradd CLI tool (#242)

--- app/views/cli.py
+++ app/views/cli.py
@@ -19,17 +19,19 @@ __email__ = "signe@atreeus.com"
 
 
 # general dependencies
-import os, sys
+import os, sys, re, datetime
 
 # import flask-related packages
 from flask import Blueprint
 from flask.cli import with_appcontext
+from werkzeug.security import generate_password_hash
 import click
 
 # libreforms dependencies
 from app import log
 from app.models import db, User
 from app.config import config
+from app.certification import generate_symmetric_key
 
 def parse_addl_fields_as_options(options_dict=config['user_registration_fields']):
     options = []
@@ -59,7 +61,7 @@ bp = Blueprint('cli', __name__)
 # STILL NEED TO ADD ADD'L ADMIN-DEFINED OPTIONS
 @bp.cli.command('useradd')
 @click.argument('username')
-@click.option('--email', prompt=True, help='email, open')
+@click.option('--email', prompt=True, help='email, open field')
 @click.password_option()
 @click.option('--organization', prompt=True, default=config['default_org'], help='organization, open field')
 @click.option('--phone', prompt=True, help='phone, open field')
@@ -69,13 +71,74 @@ bp = Blueprint('cli', __name__)
 @click.option('--group', show_default=True, default=config['default_group'], help=f'group, select from {config["groups"]}.')
 @with_appcontext
 def create_user(username, email, password, organization, phone, active, theme, group, **kwargs):
+# def create_user(**kwargs):
     """Add USERNAME to the libreforms user db."""
-    click.echo(f"{username}, {email}, {password}, {organization}, {phone}, {active}, {theme}, {group}, {[y for x,y in kwargs.items()]}")
+    # click.echo(f"{[y for x,y in kwargs.items()]}")
+    # click.echo(f"{username}, {email}, {password}, {organization}, {phone}, {active}, {theme}, {group}, {[y for x,y in kwargs.items()]}")
     # click.echo("Need to add user options and interactive user creation process.")
     
     # id = db.Column(db.Integer, primary_key=True) 
     # certificate = db.Column(db.LargeBinary())
     # created_date = db.Column(db.String(1000))
+    created_date = datetime.datetime.today().strftime("%Y-%m-%d %H:%M:%S")
+
+    if phone == "":
+        phone = None
+    
+    if email == "":
+        email = None
+
+    if organization == "":
+        email = None
+
+    error = None
+
+    if not username:
+        error = 'Username is required.'
+    elif not password:
+        error = 'Password is required.'
+    # added these per https://github.com/signebedi/libreForms/issues/122
+    # to give the freedom to set these as required fields
+    elif config['registration_email_required'] and not email:
+        error = 'Email is required.'
+    elif config['registration_phone_required'] and not phone:
+        error = 'Phone is required.'
+    elif config['registration_organization_required'] and not organization:
+        error = 'Organization is required.'
+    elif not re.fullmatch(r"^\w\w\w\w+$", username) or len(username) > 36:
+        error = 'username does not formatting standards, length 4 - 36 characters, alphanumeric and underscore characters only.'
+    elif email and not re.fullmatch(r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b', email):
+        error = 'Invalid email.' 
+    elif phone and not re.fullmatch(r'^[a-z0-9]{3}-[a-z0-9]{3}-[a-z0-9]{4}$', phone):
+        error = 'Invalid phone number (xxx-xxx-xxxx).' 
+    elif email and User.query.filter_by(email=email).first():
+        error = 'Email is already registered.' 
+    elif User.query.filter_by(username=username.lower()).first():
+        error = f'Username {username.lower()} is already registered.' 
+
+    if error:
+        click.echo(f"Failed: {error}")
+        sys.exit(2)
+
+    new_user = User(
+        email=email, 
+        username=username.lower(), 
+        password=generate_password_hash(password, method='sha256'),
+        organization=organization,
+        group=config['default_group'],
+        certificate=generate_symmetric_key(),
+        phone=phone,
+        theme='dark' if config['dark_mode'] else 'light', # we default to the application default
+        created_date=created_date,
+        active=0 if config["enable_email_verification"] else 1,
+        **kwargs, # https://stackoverflow.com/a/5710402
+    ) 
+    db.session.add(new_user)
+    db.session.commit()
+    click.echo(f"Success: created user {username}.")
+    sys.exit(0)
+
+
 
 
 @bp.cli.command('activate')
