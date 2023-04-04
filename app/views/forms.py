7b77e1c6dab6316fe3ed25e4b20af2e1d2cc39db
signebedi <signe@siftingwinnowing.com>
2023-04-03 21:45:31 -0500

    Removed: temp took out Dict field (#349)

--- app/views/forms.py
+++ app/views/forms.py
@@ -260,10 +260,11 @@ def define_webarg_form_data_types(form=False, user_group=None, args=None):
             FORM_ARGS[field] = fields.List(fields.String(),
                         required=libreforms.forms[form][field]['output_data']['required'],
                         validators=libreforms.forms[form][field]['output_data']['validators'],)
-        elif libreforms.forms[form][field]['output_data']['type'] == "dict":
-            FORM_ARGS[field] = fields.Dict(keys=fields.Str(), values=fields.Str(),
-                        required=libreforms.forms[form][field]['output_data']['required'],
-                        validators=libreforms.forms[form][field]['output_data']['validators'],)
+        # elif libreforms.forms[form][field]['output_data']['type'] == "dict":
+            # FORM_ARGS[field] = fields.Dict(keys=fields.Str(), values=fields.Str(),
+            # FORM_ARGS[field] = fields.Dict(
+            #             required=libreforms.forms[form][field]['output_data']['required'],
+            #             validators=libreforms.forms[form][field]['output_data']['validators'],)
         elif libreforms.forms[form][field]['output_data']['type'] == "int":
             FORM_ARGS[field] = fields.Int(
                         required=libreforms.forms[form][field]['output_data']['required'],
