3dcb8533fb2e92cd2c59ef8d7dfa22d3a348d6ed
signebedi <signe@siftingwinnowing.com>
2023-04-08 13:49:09 -0500

    Added: app.saml script (#7)

---
+++ app/saml.py
@@ -0,0 +1,14 @@
+""" 
+saml.py: app saml auth library
+
+
+"""
+
+__name__ = "app.saml"
+__author__ = "Sig Janoska-Bedi"
+__credits__ = ["Sig Janoska-Bedi"]
+__version__ = "1.8.0"
+__license__ = "AGPL-3.0"
+__maintainer__ = "Sig Janoska-Bedi"
+__email__ = "signe@atreeus.com"
+
