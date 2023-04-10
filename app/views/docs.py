6599b86750173304126b386ba5841d2954afc06b
signebedi <signe@siftingwinnowing.com>
2023-04-10 15:58:01 -0500

    Added: pdf download docs view (#368)

--- app/views/docs.py
+++ app/views/docs.py
@@ -15,9 +15,12 @@ __license__ = "AGPL-3.0"
 __maintainer__ = "Sig Janoska-Bedi"
 __email__ = "signe@atreeus.com"
 
+# general packages
+from xhtml2pdf import pisa
+import os
 
 # import flask-related packages
-from flask import current_app, Blueprint, render_template, request, flash, redirect, url_for, send_from_directory
+from flask import current_app, Blueprint, render_template, request, flash, redirect, url_for, send_from_directory, abort
 from flask_login import current_user, login_required
 
 # import custom packages from the current repository
@@ -38,3 +41,29 @@ def docs_home():
             type="docs",
             **standard_view_kwargs(),
         ) 
+
+@bp.route(f'/download')
+@conditional_decorator(login_required, config['require_login_for_docs'])
+def docs_download():
+
+    if not config['allow_pdf_download']:
+        return abort(404)
+
+    # here we employ a context-bound temp directory to stage this file for download, see
+    # discussion in app.tmpfiles and https://github.com/signebedi/libreForms/issues/169.
+    from app.tmpfiles import temporary_directory
+    with temporary_directory() as tempfile_path:
+
+        filename = "docs.pdf"
+        fp = os.path.join(tempfile_path, filename)
+        # Convert the HTML string to a PDF file
+        with open(fp, "wb") as output_file:
+            pisa_status = pisa.CreatePDF(config['docs_body'], dest=output_file)
+
+        if pisa_status.err:
+            flash("An error occurred while generating the PDF.", "warning")
+            return redirect(url_for("docs.docs_home"))
+        else:
+            flash("PDF successfully generated.", "success")
+            return send_from_directory(tempfile_path,
+                                    filename, as_attachment=True)
