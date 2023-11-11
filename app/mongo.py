54772dc212c79d80ab9bbe93fc73c970228840a7
signebedi <signe@siftingwinnowing.com>
2023-11-10 18:42:07 -0600

    Added: [untested] drop field from collection (#471)

--- app/mongo.py
+++ app/mongo.py
@@ -618,6 +618,18 @@ class MongoDB:
                 print(f"An error occurred: {e}")
                 return False
 
+    def remove_field_from_collection(self, collection_name, field_name):
+        with MongoClient(host=self.host, port=self.port) if not self.dbpw else MongoClient(self.connection_string) as client:
+            db = client['libreforms']
+            if collection_name not in self.collections():
+                return False
+            try:
+                collection = db[collection_name]
+                update_result = collection.update_many({}, {'$unset': {field_name: ""}})
+                return True if update_result.modified_count > 0 else False
+            except Exception as e:
+                print(f"An error occurred: {e}")
+                return False
 
     def check_connection(self):
         try:
